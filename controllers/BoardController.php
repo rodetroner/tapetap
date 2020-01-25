493d7e57703a996a97afb2f035b5391e532ac773
Szymon Hajnos <rodetroner@gmail.com>
2020-01-25 18:29:13 +0100

    Add database support

--- controllers/BoardController.php
+++ controllers/BoardController.php
@@ -2,11 +2,15 @@
 
 require_once('AppController.php');
 require_once __DIR__.'/../models/Podcast.php';
+require_once __DIR__.'/../Database.php';
 
 class BoardController extends AppController {
 
 	public function getLatestPodcasts()
 	{
+		$database = new Database();
+		$database->connect();
+
 		$podcast1 = new Podcast();
 		$podcast2 = new Podcast();
 		$podcast3 = new Podcast();
