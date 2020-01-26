dd06c7597d0708ca01e2cdecb56720f16621420c
Szymon Hajnos <rodetroner@gmail.com>
2020-01-26 15:54:06 +0100

    Remove unused menu items

--- views/BoardController/board.php
+++ views/BoardController/board.php
@@ -21,22 +21,13 @@
 				<img src="../public/img/logo-transparent.svg">
 			</div>
 			<div class="navitem">
-				HOME
+				<a href="?page=board">HOME</a>
 			</div>
 			<div class="navitem">
 				QUEUE
 			</div>
 			<div class="navitem">
 				SUBSCRIBED
-			</div>
-			<div class="navitem">
-				EXPLORE
-			</div>
-			<div class="navitem">
-				SEARCH
-			</div>
-			<div class="navitem">
-				SETTINGS
 			</div>
 			<div class="navitem">
 				<a href="?page=logout">LOGOUT</a>
